3dea91eeae70ee5da282ed5f7fb9865984fee8ca
Amitt <08sharma.amit@gmail.com>
2021-04-21 12:48:40 +0530

    Help service added

---
+++ Backend/help/src/test/java/com/uncovid/help/HelpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uncovid.help;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
